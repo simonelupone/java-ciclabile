b82a0f5325ac4f165863b7e47abba616b0b26cae
simonelupone <simone.lupone@gmail.com>
2025-08-26 03:25:00 +0200

    feat:[BREAKING CHANGE]: update getElementoSuccessivo with infinie loop every time the method is called. update hasAncoraElementi to be consistent add method to add an element

--- src/App.java
+++ src/App.java
@@ -4,44 +4,73 @@ public class App {
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, World!");
 
-        int[] array = { 1, 2, 3, 4, 5 };
+        int[] array = { 16, 77, 91, 447, 50 };
         Ciclabile nuovoArray = new Ciclabile(array);
 
+        nuovoArray.addElement(6);
+
+        System.out.println(nuovoArray.toString());
+
         System.out.print("Elemento Current: " + nuovoArray.getCurrent() + " | ");
-        System.out.print("getElementoSuccessivo(): " + nuovoArray.getElementoSuccessivo());
+        System.out.print("getElementoSuccessivo(): " +
+                nuovoArray.getElementoSuccessivo());
         System.out.println("\nhas next--->" + nuovoArray.hasAncoraElementi());
 
         System.out.println("--------------------------------------------------------");
 
         System.out.print("Elemento Current: " + nuovoArray.getCurrent() + " | ");
-        System.out.print("getElementoSuccessivo(): " + nuovoArray.getElementoSuccessivo());
+        System.out.print("getElementoSuccessivo(): " +
+                nuovoArray.getElementoSuccessivo());
         System.out.println("\nhas next--->" + nuovoArray.hasAncoraElementi());
 
         System.out.println("--------------------------------------------------------");
 
         System.out.print("Elemento Current: " + nuovoArray.getCurrent() + " | ");
-        System.out.print("getElementoSuccessivo(): " + nuovoArray.getElementoSuccessivo());
+        System.out.print("getElementoSuccessivo(): " +
+                nuovoArray.getElementoSuccessivo());
         System.out.println("\nhas next--->" + nuovoArray.hasAncoraElementi());
 
         System.out.println("--------------------------------------------------------");
 
         System.out.print("Elemento Current: " + nuovoArray.getCurrent() + " | ");
-        System.out.print("getElementoSuccessivo(): " + nuovoArray.getElementoSuccessivo());
+        System.out.print("getElementoSuccessivo(): " +
+                nuovoArray.getElementoSuccessivo());
         System.out.println("\nhas next--->" + nuovoArray.hasAncoraElementi());
 
         System.out.println("--------------------------------------------------------");
 
         System.out.print("Elemento Current: " + nuovoArray.getCurrent() + " | ");
-        System.out.print("getElementoSuccessivo(): " + nuovoArray.getElementoSuccessivo());
+        System.out.print("getElementoSuccessivo(): " +
+                nuovoArray.getElementoSuccessivo());
         System.out.println("\nhas next--->" + nuovoArray.hasAncoraElementi());
 
         System.out.println("--------------------------------------------------------");
 
         System.out.print("Elemento Current: " + nuovoArray.getCurrent() + " | ");
-        System.out.print("getElementoSuccessivo(): " + nuovoArray.getElementoSuccessivo());
+        System.out.print("getElementoSuccessivo(): " +
+                nuovoArray.getElementoSuccessivo());
         System.out.println("\nhas next--->" + nuovoArray.hasAncoraElementi());
 
         System.out.println("--------------------------------------------------------");
+
+        System.out.print("Elemento Current: " + nuovoArray.getCurrent() + " | ");
+        System.out.print("getElementoSuccessivo(): " +
+                nuovoArray.getElementoSuccessivo());
+        System.out.println("\nhas next--->" + nuovoArray.hasAncoraElementi());
+
+        System.out.println("--------------------------------------------------------");
+
+        System.out.print("Elemento Current: " + nuovoArray.getCurrent() + " | ");
+        System.out.print("getElementoSuccessivo(): " +
+                nuovoArray.getElementoSuccessivo());
+        System.out.println("\nhas next--->" + nuovoArray.hasAncoraElementi());
+
+        System.out.println("--------------------------------------------------------");
+
+        nuovoArray.addElement(586);
+
+        System.out.println(nuovoArray.toString());
+
     }
 
 }
--- src/org/collections/java/Ciclabile.java
+++ src/org/collections/java/Ciclabile.java
@@ -1,5 +1,6 @@
 package org.collections.java;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ciclabile {
@@ -12,6 +13,11 @@ public class Ciclabile {
         this.listaInteri = listaInteri;
         if (listaInteri.length > 0)
             current = 0;
+    }
+
+    public Ciclabile() {
+        this.listaInteri = new int[0];
+        this.current = 0;
     }
 
     public int[] getListaInteri() {
@@ -35,43 +41,76 @@ public class Ciclabile {
     }
 
     public boolean hasAncoraElementi() {
-        if (listaInteri.length == 0) {
+        if (listaInteri.length <= 1) {
             return false;
         }
-        return listaInteri.length > current;
+        // return listaInteri.length > current;
+
+        // ogni volta che modulo di current per length è != 0 da false,
+        // se no vuol dire che ci sono elementi
+        return current % listaInteri.length != 0;
     }
 
     public int getElementoSuccessivo() throws ArrayIndexOutOfBoundsException {
 
-        if (current >= listaInteri.length) {
+        // if (current >= listaInteri.length) {
 
-            System.out.println("Hai raggiunto la fine dell'array.");
-            System.out.println("Vuoi azzerare il contatore? (s/n))");
+        // System.out.println("Hai raggiunto la fine dell'array.");
+        // System.out.println("Vuoi azzerare il contatore? (s/n))");
 
-            String choice = sc.nextLine().toLowerCase();
+        // String choice = sc.nextLine().toLowerCase();
 
-            switch (choice) {
-                case "s":
-                    current = 0;
-                    break;
-                case "n":
-                    throw new ArrayIndexOutOfBoundsException(
-                            "Indice più grnade dell'array. Per continuare il ciclo devi resettare il contatore!");
-                default:
-                    System.out.println("Scegli (s/n)");
-                    break;
-            }
+        // switch (choice) {
+        // case "s":
+        // current = 0;
+        // break;
+        // case "n":
+        // throw new ArrayIndexOutOfBoundsException(
+        // "Indice più grnade dell'array. Per continuare il ciclo devi resettare il
+        // contatore!");
+        // default:
+        // System.out.println("Scegli (s/n)");
+        // break;
+        // }
 
-            sc.close();
+        // sc.close();
+        // }
+
+        // int number = listaInteri[current];
+
+        // System.out.println("number: " + number);
+
+        // current++;
+
+        // return number;
+
+        // oppure senza fare la domanda e ciclando automaticamene
+        if (listaInteri.length == 0) {
+            throw new ArrayIndexOutOfBoundsException("Array vuoto!");
         }
 
-        int number = listaInteri[current];
-
-        System.out.println("number: " + number);
-
+        // il modulo di current per la lunghezza dell'array da sempre
+        // il resto progressivo fino a tornare 0
+        // (tipo array lungo 4 si resetterà a 4, 8, 12 perchè resto 0)
+        int elemento = listaInteri[current % listaInteri.length];
         current++;
-
-        return number;
+        return elemento;
     }
 
+    public void addElement(int element) {
+        int[] nuovoArray = new int[listaInteri.length + 1];
+        for (int i = 0; i < listaInteri.length; i++) {
+            nuovoArray[i] = listaInteri[i];
+        }
+        nuovoArray[nuovoArray.length - 1] = element;
+        listaInteri = nuovoArray;
+    }
+
+    @Override
+    public String toString() {
+        return "Ciclabile{" +
+                "array=" + Arrays.toString(listaInteri) +
+                ", posizione=" + current +
+                '}';
+    }
 }
